279b2db809abcb6103251f6879d53faa9bd3b2ca
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-08-26 14:47:12 -0400

    Initial setip

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # users-rect-app
+
+```bash
+git clone https://github.com/shibut12/users-rect-app.git
+cd user-react-app
+npm install
+npm start
+
+# load http://localhost:3000 on a browser
+```
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>user List</h2>
+        <hr />
+        <h2>User Details</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
