f78664d86ee97c20027803b896b267d4fc6aa696
Shibu Thannikkunnath <shibu@hotmail.co.in>
2018-08-26 15:44:06 -0400

    Created a reducer to respond ro selected user action

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import UserReducer from './users-reducer';
+import ActiveuserReducer from './active-user-reducer';
 
 const allReducers = combineReducers({
-    users: UserReducer
+    users: UserReducer,
+    activeUser: ActiveuserReducer
 })
 
 export default allReducers;
